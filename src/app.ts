13492f8ea27f97f3dcd07311a22920d065bf961d
Yared Tsegaye <tsyared@gmail.com>
2023-06-06 09:34:12 +0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,52 @@
+import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+// modules
+import { corsUrl, environment } from '@config';
+import Logger from '@core/Logger';
+import {
+    NotFoundError,
+    ApiError,
+    InternalError,
+    ErrorType
+} from '@core/ApiError';
+import apps from '@apps';
+import '@database';
+
+const app = express();
+app.use(express.json({ limit: '10mb' }));
+app.use(
+    express.urlencoded({ limit: '10mb', extended: true, parameterLimit: 50000 })
+);
+app.use(morgan('tiny'));
+app.use(cors({ origin: corsUrl, optionsSuccessStatus: 200 }));
+
+process.on('uncaughtException', (e) => {
+    Logger.error(e);
+});
+
+// Routes
+app.use('/', apps);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => next(new NotFoundError()));
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof ApiError) {
+        ApiError.handle(err, res);
+        if (err.type === ErrorType.INTERNAL)
+            Logger.error(
+                `500 - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`
+            );
+    } else {
+        Logger.error(
+            `500 - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`
+        );
+        Logger.error(err);
+        if (environment === 'development') {
+            return res.status(500).send(err);
+        }
+        ApiError.handle(new InternalError(), res);
+    }
+});
+export default app;
